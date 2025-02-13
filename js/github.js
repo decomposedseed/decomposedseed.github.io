af5e4468c258b4d04ed9f01d27eb12cb8234a8c0
decomposedseed <kmaldonado2019@udec.cl>
2025-02-13 14:44:00 -0300

    Add files via upload

---
+++ js/github.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.classList.add('loaded');
+});
+
+function open_card() {
+  document.getElementById('front').className = 'open-card';
+}
+
+function close_card() {
+  document.getElementById('front').className = '';
+}
+
+var button=document.getElementById("front");
+button.addEventListener("click", open_card, false);
+
+var button=document.getElementById("inside");
+button.addEventListener("click", close_card, false);
